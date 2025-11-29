49972fa199fd33d8e09cd106128a5c28194aa72b
nic340 <nicolediones5@gmail.com>
2025-11-29 08:08:01 -0800

    Add User, Product, and Order models

--- models.py
+++ models.py
@@ -1,24 +1,26 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import SQLModel, Field
 
-# 1. Store Model
-class Store(SQLModel, table=True):
+
+# 1. USER MODEL
+class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    address: str
+    email: str
 
-# 2. Transaction Model
-class Transaction(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    gcash_number: str
-    amount: float
-    status: str
-    # Foreign Key linking to Store
-    store_id: int | None = Field(default=None, foreign_key="store.id")
 
-# 3. Fee Model
-class Fee(SQLModel, table=True):
+# 2. PRODUCT MODEL
+class Product(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    fee_percentage: float
-    fee_amount: float
-    # Foreign Key linking to Transaction (One-to-One concept)
-    transaction_id: int | None = Field(default=None, foreign_key="transaction.id")
+    name: str
+    price: float
+    stock: int
+
+
+# 3. ORDER MODEL
+class Order(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    quantity: int
+    total_price: float
+    product_id: int | None = Field(default=None, foreign_key="product.id")
+    user_id: int | None = Field(default=None, foreign_key="user.id")
+
